5ae426f234c661da10bdd377456702c9809c2bd4
SadlyHermanto <Sadlyhermanto45@gmail.com>
2021-06-06 21:51:35 +0800

    'ubah'

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const tombolCari = document.querySelector('.tombol-cari')
 const keyword = document.querySelector('.keyword')
 const container = document.querySelector('.container')
 
-//keyword event handler
 keyword.addEventListener('keyup', function () {
   fetch('ajax_cari.php?keyword=' + keyword.value)
     .then(response => response.text())
